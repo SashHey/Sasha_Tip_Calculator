fba3f6124df8ee72fca278d13831f6c46345843f
SashHey <60923007+SashHey@users.noreply.github.com>
2023-09-06 19:16:25 +0200

    Added foundation variables and arrays

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,38 @@
-function App(){
-    return (
-        <div>
-            <h1>Welcome !</h1>
-        </div>
-    );
-}
+import React, { useState } from 'react';
+
+const bill = 0;
+const customTip = 0;
+const people = 0;
+const tipAmount = 0;
+const tipTotal = 0;
+const reset = false;
+
+const tip = [
+    {
+        id: 'p1',
+        title: '5%',
+        amount: 0.05,
+    },
+    {
+        id: 'p2',
+        title: '10%',
+        amount: 0.1,
+    },
+    {
+        id: 'p3',
+        title: '15%',
+        amount: 0.15,
+    },
+    {
+        id: 'p4',
+        title: '25%',
+        amount: 0.25,
+    },
+    {
+        id: 'p5',
+        title: '50%',
+        amount: 0.5,
+    },
+];
 
 export default App;
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import ReactDom from 'react-dom/client';
 import 'CSS/index.css';
 import app from "./app"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<app/>);
